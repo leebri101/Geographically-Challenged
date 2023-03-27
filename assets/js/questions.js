afeeb22b213238940cd4bfc95284d5434e8ff365
leebri101 <brian-lee@live.co.uk>
2023-03-27 20:47:29 +0000

    24 questions on geography quiz has been added

---
+++ assets/js/questions.js
@@ -0,0 +1,255 @@
+    /* jshint esversion: 8 */
+    const quizQuestions = [{
+        questionNum: "1",
+        questionText: "Ninety percent of the Earth's population lives in which hemisphere?",
+        choices: [
+        "The Northern Hemisphere",
+        "The Southern Hemisphere",
+        "The Eastern Hemisphere",
+        "The Western Hemisphere"
+        ],
+        correctAns: "The Northern Hemisphere"
+    },
+    {
+    questionNum: "2",
+    questionText: "How many time zones does Australia have?",
+    choices: [
+        "6",
+        "5",
+        "4",
+        "3"
+        ],
+        correctAns: "3"
+    },
+    {
+    questionNum: "3",
+    questionText: "Which continent is in all four hemispheres?",
+    choices: [
+        "Africa",
+        "South America",
+        "Asia",
+        "Europe"
+        ],
+        correctAns: "Africa"
+    },
+    {
+    questionNum: "4",
+    questionText: "What is the order of the largest oceans on Earth?",
+    choices: [
+        "Atlantic, Pacific, Indian, Arctic, and Southern",
+        "Arctic, Southern, Indian, Atlantic and Pacific",
+        "Pacific, Atlantic, Indian, Southern and Arctic",
+        "Indian, Artic, Atlantic, Southern, and Pacific"
+        ],
+        correctAns: "Pacific, Atlantic, Indian, Southern and the Arctic"
+    },
+    {
+    questionNum: "5",
+    questionText: "Where is the world's largest desert located?",
+    choices: [
+        "Arctic Desert",
+        "Sahara Desert",
+        "Arabian Desert",
+        "Antarctic Desert"
+        ],
+        correctAns: "Antarctic Desert"
+    },
+    {
+    questionNum: "6",
+    questionText: "What is the capital city of Spain?",
+    choices: [
+        "Madrid",
+        "Valencia",
+        "Barcelona",
+        "Seville "
+        ],
+        correctAns: "Madrid"
+    },
+    {
+    questionNum: "7",
+    questionText: "What is the capital city of Australia?",
+    choices: [
+        "Canberra",
+        "Sydney",
+        "Melbourne",
+        "Brisbane"
+        ],
+        correctAns: "Canberra"
+    },
+    {
+    questionNum: "8",
+    questionText: "What is the capital city of South Korea",
+    choices: [
+        "Busan",
+        "Incheon",
+        "Seoul",
+        "Daegu"
+        ],
+        correctAns: "Seoul"
+    },
+    {
+    questionNum: "9",
+    questionText: "What is the capital city of New Zealand?",
+    choices: [
+        "Wellington",
+        "Auckland",
+        "Christchurch",
+        "Dunedin"
+        ],
+        correctAns: "Wellington"
+    },
+    {
+    questionNum: "10",
+    questionText: "What country has the largest population in the world?",
+    choices: [
+        "China",
+        "The United States",
+        "Indonesia",
+        "India"
+        ],
+        correctAns: "China"
+    },
+    {
+    questionNum: "11",
+    questionText: "",
+    choices: [
+        "Monaco",
+        "Nauru",
+        "Macau",
+        "Vatican City"
+        ],
+        correctAns: "Vatican City"
+    },
+    {
+    questionNum: "12",
+    questionText: "What is the largest ocean in the world?",
+    choices: [
+        "Atlantic Ocean",
+        "Artic Ocean",
+        "Indian Ocean",
+        "Pacfic Ocean"
+        ],
+        correctAns: "Pacfic Ocean"
+    },
+    {
+    questionNum: "13",
+    questionText: "Which city in the Philippines is the most densely populated?",
+    choices: [
+        "Caloocan",
+        "Manila",
+        "Pateros",
+        "Mandaluyong"
+        ],
+        correctAns: "Manila"
+    },
+    {
+    questionNum: "14",
+    questionText: "",
+    choices: [
+        "Mount Fuji",
+        "Popocatépetl",
+        "Mauna Loa",
+        "Kilimanjaro"
+        ],
+        correctAns: "Mauna Loa located in Hawaii"
+    },
+    {
+    questionNum: "15",
+    questionText: "What is the name of China’s largest river?",
+    choices: [
+        "The Yangshe River",
+        "The Yangsea River",
+        "The Yangtze River",
+        "The Yangze River"
+        ],
+        correctAns: "The Yangtze River"
+    },
+    {
+    questionNum: "16",
+    questionText: "What is the tallest moutain the UK?",
+    choices: [
+        "Carn Eige",
+        "Snowdon - Yr Wyddfa",
+        "Carrauntoohil",
+        "Ben Nevis"
+        ],
+        correctAns: "Ben Nevis"
+    },
+    {
+    questionNum: "17",
+    questionText: "How many States does the United States consist of?",
+    choices: [
+        "50",
+        "52",
+        "49",
+        "48"
+        ],
+        correctAns: "50"
+    },
+    {
+    questionNum: "18",
+    questionText: "How many countries are there in the United Kingdom?",
+    choices: [
+        "4",
+        "5",
+        "3",
+        "2"
+        ],
+        correctAns: ""
+    },
+    {
+    questionNum: "19",
+    questionText: "How many time zones does Russia have?",
+    choices: [
+        "11",
+        "13",
+        "10",
+        "A lot"
+        ],
+        correctAns: "11"
+    },
+    {
+    questionNum: "20",
+    questionText: "What is the name of the largest city in Australia?",
+    choices: [
+        "Brisbane",
+        "Sydney",
+        "Melbourne",
+        "Perth"
+        ],
+        correctAns: "Brisbane"
+    },
+    {
+    questionNum: "21",
+    questionText: "What country is known to have the best quality tap water?",
+    choices: [
+        "France",
+        "Switzerland",
+        "Norway",
+        "Luxembourg"
+        ],
+        correctAns: "Switzerland"
+    },
+    {
+    questionNum: "23",
+    questionText: "In what ocean is the Bermuda Triangle located?",
+    choices: [
+        "Pacific Ocean",
+        "Indian Ocean",
+        "Atlantic Ocean",
+        "Arctic Ocean"
+        ],
+        correctAns: "Atlantic Ocean"
+    },
+    {
+    questionNum: "24",
+    questionText: "The Grand Canyon is located in which US state?",
+    choices: [
+        "Florida",
+        "Texas",
+        "Arizona",
+        "Mississippi"
+        ],
+        correctAns: "Arizona"
+    }
+];
