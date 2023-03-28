91ab7bbd88102e3f4686c27dc0cc366f3727f3f1
leebri101 <brian-lee@live.co.uk>
2023-03-28 18:21:05 +0000

    Added basic functionality & variables  to Java Script file

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+/* jshint esversion: 8 */
+
+// variables 
+const quizLength = 10;
+let correctNum = 0;
+let questionCount = 0;
+const footerSection = document.getElementById("footer");
+
+
+
+function startNewGame(){
+
+}
+
+function leave(){
+
+}
+
+
+function endOfQuiz(){
+    footerSection.style.display = "block"
+}
+function checkAnswer(){
+
+}
