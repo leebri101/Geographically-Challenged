c3a1683a436d2cf39500f717d38ddbca40143ad3
leebri101 <brian-lee@live.co.uk>
2023-03-29 17:28:18 +0000

    Added basic script for start game section

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,21 +6,32 @@ let correctNum = 0;
 let questionCount = 0;
 const footerSection = document.getElementById("footer");
 const landingSection = document.getElementById("landing");
-const playButton = document.getElementById("play-btn")
-
+const playButton = document.getElementById("play-btn");
+const newGameSection = document.getElementById("new-game");
+const playerName = document.getElementById("player-name");
+const startQuizBtn = document.getElementById("start-quiz-btn");
+const leaveQuizBtn = documen.getElementById("leave-quiz-btn");
 
 function startNewGame(){
-quiz
+    landingSection.style.display = "none";
+    newGameSection.style.display = "inline-flex";
+    playerName.focus();
 }
 
-function leave(){
+playButton.addEventListener("click", startNewGame);
 
+function leaveQuiz(){
+    playerName.value = '';
+    landingSection.style.display = "inline-flex";
+    newGameSection.style.display = "none";
 }
 
-
+leaveQuizBtn.addEventListener("click", leaveQuiz);
+/*
 function endOfQuiz(){
     footerSection.style.display = "block";
 }
 function checkAnswer(){
 
 }
+*/
